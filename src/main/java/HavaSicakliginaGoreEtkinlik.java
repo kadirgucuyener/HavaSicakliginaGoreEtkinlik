3d34787cfde16668918bbe56bf3225728fa67585
Kadir <kadirgucuyener35@gmail.com>
2022-11-28 16:45:03 +0200

    Initial commit

---
+++ src/main/java/HavaSicakliginaGoreEtkinlik.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HavaSicakliginaGoreEtkinlik {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Sıcaklığı giriniz:");
+        heat = input.nextInt();
+        if(heat < 5){
+            System.out.println("Kayak yapabilirsiniz!");
+        } else if ((heat>=5) && (heat<=25)) {
+            if (heat >= 10 && heat <= 15) {
+                System.out.println("Sinemaya veya Pikniğe gidebilirsiniz!");
+            } else if (heat >= 5 && heat <= 15) {
+                System.out.println("Sinemaya gidebilirsiniz!");
+            } else if ((heat >= 10) && (heat <= 25)) {
+                System.out.println("Pikniğe gidebilirsiniz!");
+            }
+        }    else if(heat>25) {
+            System.out.println( "Yüzmeye gidebilirsiniz!");
+        }
+    }
+}
